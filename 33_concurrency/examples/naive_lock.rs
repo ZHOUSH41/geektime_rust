50cd99606c73557c82e89fd0edc17370140ae20a
ZHOUSH41 <624061566@qq.com>
2022-04-13 22:25:36 +0800

    33 init

---
+++ 33_concurrency/examples/naive_lock.rs
@@ -0,0 +1,58 @@
+use std::{cell::RefCell, fmt, sync::Arc, thread};
+struct Lock<T> {
+    locked: RefCell<bool>,
+    data: RefCell<T>,
+}
+
+impl<T> fmt::Debug for Lock<T>
+where
+    T: fmt::Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Lock<{:?}>", self.data.borrow())
+    }
+}
+// SAFETY: 我们确信 Lock<T> 很安全，可以在多个线程中共享
+unsafe impl<T> Sync for Lock<T> {}
+
+impl<T> Lock<T> {
+    pub fn new(data: T) -> Self {
+        Self {
+            locked: RefCell::new(false),
+            data: RefCell::new(data),
+        }
+    }
+
+    pub fn lock(&self, op: impl FnOnce(&mut T)) {
+        // 如果没拿到锁，就一直 spin
+        while *self.locked.borrow() != false {}
+
+        // 拿到，赶紧加锁
+        *self.locked.borrow_mut() = true;
+
+        // 开始干活
+        op(&mut self.data.borrow_mut());
+
+        // 解锁
+        *self.locked.borrow_mut() = false;
+    }
+}
+
+fn main() {
+    let data = Arc::new(Lock::new(0));
+    let data1 = data.clone();
+
+    let t1 = thread::spawn(move || {
+        data1.lock(|x| *x += 10);
+    });
+
+    let data2 = data.clone();
+    let t2 = thread::spawn(move || {
+        data2.lock(|x| *x += 10);
+    });
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+
+    println!("{:?}", data);
+}
