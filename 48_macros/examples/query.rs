47190e061e60d61f3501044ad1b674bdf12221fa
ZHOUSH41 <624061566@qq.com>
2022-04-22 09:23:46 +0800

    proc macros

--- 48_macros/examples/query.rs
+++ 48_macros/examples/query.rs
@@ -1,6 +1,6 @@
 use macros::query;
 
 fn main() {
-    query!(SELECT * FROM users WHERE age > 10);
+    // query!(SELECT * FROM users WHERE age > 10);
     query!(SELECT * FROM users u JOIN (SELECT * from profiles p) WHERE u.id = p.id and u.age > 10);
 }
