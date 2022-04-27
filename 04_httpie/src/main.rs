99544655a0dbacfd218dd225a5c7952456ee5833
ZHOUSH41 <624061566@qq.com>
2022-04-27 23:24:44 +0800

    rewrite v1.0

--- 04_httpie/src/main.rs
+++ 04_httpie/src/main.rs
@@ -1,35 +1,28 @@
-use std::{collections::HashMap, str::FromStr};
-
-use anyhow::{anyhow, Result};
 use clap::Parser;
-use reqwest::{header, Client, Response, Url};
+use reqwest::Url;
+use anyhow::Result;
 
-#[derive(Parser, Debug)]
-#[clap(version = "1.0")]
+#[derive(Debug, Parser)]
+#[clap(version = "1.0", author = "Me")]
 struct Opts {
     #[clap(subcommand)]
     subcmd: SubCommand,
 }
-
-#[derive(Parser, Debug)]
+#[derive(Debug, Parser)]
 enum SubCommand {
     Get(Get),
     Post(Post),
 }
 
-/// feed get with an url and we will retrieve the response for you
-#[derive(Parser, Debug)]
 struct Get {
-    /// HTTP 请求的 URL
-    #[clap(parse(try_from_str = parse_url))]
+    #[clap(parse(try_from_str=parse_url))]
     url: String,
 }
 
-#[derive(Parser, Debug)]
 struct Post {
-    #[clap(parse(try_from_str = parse_url))]
+    #[clap(parse(try_from_str=parse_url))]
     url: String,
-    #[clap(parse(try_from_str = parse_kv_pair_url))]
+    #[clap(parse(from_str=parse_kv_pair))]
     body: Vec<KvPair>,
 }
 
@@ -39,57 +32,17 @@ struct KvPair {
     v: String,
 }
 
-impl FromStr for KvPair {
-    type Err = anyhow::Error;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut split = s.split("=");
-        let err = || anyhow!(format!("Failed to parse {}", s));
-        Ok(Self {
-            k: (split.next().ok_or_else(err)?).to_string(),
-            v: (split.next().ok_or_else(err)?).to_string(),
-        })
-    }
-}
 
-fn parse_kv_pair_url(s: &str) -> Result<KvPair> {
-    s.parse()
-}
 fn parse_url(s: &str) -> Result<String> {
     let _url: Url = s.parse()?;
-    Ok(s.into())
+    Ok(s.into()) 
 }
+
+fn 
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let opts: Opts = Opts::parse();
-    let mut headers = header::HeaderMap::new();
-    headers.insert("X-POWERED-BY", "Rust".parse()?);
-    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
-    let client = reqwest::Client::builder()
-        .default_headers(headers)
-        .build()?;
-
-    let result = match opts.subcmd {
-        SubCommand::Get(ref args) => get(client, args).await?,
-        SubCommand::Post(ref args) => post(client, args).await?,
-    };
-
-    Ok(result)
-}
-
-async fn get(client: Client, args: &Get) -> Result<()> {
-    let resp = client.get(&args.url).send().await?;
-    println!("{:?}", resp.text().await?);
+async fn main() -> Result<()>{
     Ok(())
 }
 
-async fn post(client: Client, args: &Post) -> Result<()> {
-    let mut body = HashMap::new();
-    for pair in args.body.iter() {
-        body.insert(&pair.k, &pair.v);
-    }
-    let resp = client.post(&args.url).json(&body).send().await?;
-    println!("{:?}", resp.text().await?);
-    Ok(())
-}
