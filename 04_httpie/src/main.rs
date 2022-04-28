226f80a82147c43ba1b7ac6612bb672381c84e9e
ZHOUSH41 <624061566@qq.com>
2022-04-28 09:51:51 +0800

    04 rewrite v1.1

--- 04_httpie/src/main.rs
+++ 04_httpie/src/main.rs
@@ -1,6 +1,16 @@
+use std::{str::FromStr, collections::HashMap};
+
+use anyhow::{anyhow, Result};
 use clap::Parser;
-use reqwest::Url;
-use anyhow::Result;
+use colored::Colorize;
+use mime::Mime;
+use reqwest::{header, Client, Response, Url};
+use syntect::{
+    easy::HighlightLines,
+    highlighting::{Style, ThemeSet},
+    parsing::SyntaxSet,
+    util::{as_24_bit_terminal_escaped, LinesWithEndings},
+};
 
 #[derive(Debug, Parser)]
 #[clap(version = "1.0", author = "Me")]
@@ -8,21 +18,31 @@ struct Opts {
     #[clap(subcommand)]
     subcmd: SubCommand,
 }
+
+// 子命令对应不同的HTTP方法，目前只支持 get/post
 #[derive(Debug, Parser)]
 enum SubCommand {
     Get(Get),
     Post(Post),
+    // 我们暂且不支持其它 HTTP 方法
 }
 
+// get 子命令
+#[derive(Debug, Parser)]
 struct Get {
+    // HTTP 请求的 URL
     #[clap(parse(try_from_str=parse_url))]
     url: String,
 }
 
+// post 子命令。需要输入一个 url，和若干个可选的 key=value，用于提供 json body
+#[derive(Debug, Parser)]
 struct Post {
+    // HTTP 请求的 URL
     #[clap(parse(try_from_str=parse_url))]
     url: String,
-    #[clap(parse(from_str=parse_kv_pair))]
+    // HTTP 请求的 body
+    #[clap(parse(try_from_str=parse_kv_pair))]
     body: Vec<KvPair>,
 }
 
@@ -32,17 +52,142 @@ struct KvPair {
     v: String,
 }
 
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
 
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // 使用 = 进行split，这会得到一个迭代器
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        // 
+        Ok(Self {
+            k: (split.next().ok_or_else(err)?).to_string(),
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
 
 fn parse_url(s: &str) -> Result<String> {
     let _url: Url = s.parse()?;
     Ok(s.into()) 
 }
 
-fn 
-
-#[tokio::main]
-async fn main() -> Result<()>{
-    Ok(())
+/// 因为我们为 KvPair 实现了 FromStr，这里可以直接 s.parse() 得到 KvPair
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    s.parse()
 }
 
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    Ok(print_resp(resp).await?)
+}
+#[tokio::main]
+async fn main() -> Result<()> {
+    let opts = Opts::parse();
+    let mut headers = header::HeaderMap::new();
+    // 为我们的 http 客户端添加一些缺省的 HTTP 头
+    headers.insert("X-POWERED-BY", "Rust".parse()?);
+    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+    let result = match opts.subcmd {
+        SubCommand::Get(ref args) => get(client, args).await?,
+        SubCommand::Post(ref args) => post(client, args).await?,
+    };
+    Ok(result)
+}
+
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
+}
+// 打印服务器版本号 + 状态码
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status);
+}
+
+// 打印服务器返回的 HTTP header
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+
+    println!();
+}
+
+/// 打印服务器返回的 HTTP body
+fn print_body(m: Option<Mime>, body: &str) {
+    match m {
+        // 对于 "application/json" 我们 pretty print
+        Some(v) if v == mime::APPLICATION_JSON => print_syntect(body, "json"),
+        Some(v) if v == mime::TEXT_HTML => print_syntect(body, "html"),
+
+        // 其它 mime type，我们就直接输出
+        _ => println!("{}", body),
+    }
+}
+/// 将服务器返回的 content-type 解析成 Mime 类型
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+fn print_syntect(s: &str, ext: &str) {
+    // Load these once at the start of your program
+    let ps = SyntaxSet::load_defaults_newlines();
+    let ts = ThemeSet::load_defaults();
+    let syntax = ps.find_syntax_by_extension(ext).unwrap();
+    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+    for line in LinesWithEndings::from(s) {
+        let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
+        let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
+        print!("{}", escaped);
+    }
+}
+// 仅在 cargo test 时才编译
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_url_works() {
+        assert!(parse_url("abc").is_err());
+        assert!(parse_url("http://abc.xyz").is_ok());
+        assert!(parse_url("https://httpbin.org/post").is_ok());
+    }
+
+    #[test]
+    fn parse_kv_pair_works() {
+        assert!(parse_kv_pair("a").is_err());
+        assert_eq!(
+            parse_kv_pair("a=1").unwrap(),
+            KvPair {
+                k: "a".into(),
+                v: "1".into()
+            }
+        );
+
+        assert_eq!(
+            parse_kv_pair("b=").unwrap(),
+            KvPair {
+                k: "b".into(),
+                v: "".into()
+            }
+        );
+    }
+}
