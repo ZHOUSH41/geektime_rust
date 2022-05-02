c8f5f68471fcf6ceca5a4fb8abbeb37543755b90
ZHOUSH41 <624061566@qq.com>
2022-05-02 23:19:28 +0800

    0.1.1

--- 05_thumbor/src/main.rs
+++ 05_thumbor/src/main.rs
@@ -1,2 +1,22 @@
 // rewrite Thumbor to use rust
 // version 0.1.0
+
+mod pb;
+
+// 解析出来的图片处理的参数
+struct ImageSpec {
+    specs: Vec<Spec>,
+}
+
+// 每个参数是我们支持的某种处理方式
+enum Spec {
+    Resize(Resize),
+    // Crop(Crop),
+}
+
+
+// 处理图片的 resize
+struct Resize {
+    width: u32,
+    height: u32,
+}
